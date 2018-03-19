f863493f5471dc1bc5590cceaa2c7714f3d3564d
Christian Marienfeld <post@chrisland.de>
2018-03-19 13:54:12 +0100

    -

---
+++ platforms/browser/www/js/tasks.js
@@ -0,0 +1,19 @@
+var cPagerTask = (function(){
+	var tasks = {
+
+		initialize: function (page, content, e, dom) {
+
+			console.log('---initialize');
+			
+			// BEFORE dom changed
+			
+			return function () {
+				
+				// AFTER dom changed
+				
+			};
+		}
+
+	};
+	return tasks;
+}());
---
+++ platforms/browser/www/tmpl/home.tpl
@@ -0,0 +1,2 @@
+<h1>cHapp - blank app</h1>
+<h3>Home</h3>
